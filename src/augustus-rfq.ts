cef4b5f65e3d362fa89bd79c0d08b948aed389f9
Harsh Agrawal <harshagrawal.1312@gmail.com>
2022-06-23 11:29:40 +0530

    update: to paraswap-rfq-subgraph

--- src/augustus-rfq.ts
+++ src/augustus-rfq.ts
@@ -1,65 +1,72 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  AugustusRFQ,
-  OrderCancelled,
-  OrderFilled,
-  OrderFilledNFT
-} from "../generated/AugustusRFQ/AugustusRFQ"
-import { ExampleEntity } from "../generated/schema"
+import { BigInt, log, Bytes } from "@graphprotocol/graph-ts";
+import { OrderFilled, OrderFilledNFT } from "../generated/AugustusRFQ/AugustusRFQ";
+import { FilledOrder, FilledOrderNFT } from "../generated/schema";
 
-export function handleOrderCancelled(event: OrderCancelled): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.orderHash = event.params.orderHash
-  entity.maker = event.params.maker
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.FILLED_ORDER(...)
-  // - contract.RFQ_LIMIT_NFT_ORDER_TYPEHASH(...)
-  // - contract.RFQ_LIMIT_ORDER_TYPEHASH(...)
-  // - contract.UNFILLED_ORDER(...)
-  // - contract.getRemainingOrderBalance(...)
-  // - contract.partialFillOrder(...)
-  // - contract.partialFillOrderNFT(...)
-  // - contract.partialFillOrderWithTarget(...)
-  // - contract.partialFillOrderWithTargetNFT(...)
-  // - contract.partialFillOrderWithTargetPermit(...)
-  // - contract.partialFillOrderWithTargetPermitNFT(...)
-  // - contract.remaining(...)
+export function handleOrderFilled(event: OrderFilled): void {
+  let filledOrder = new FilledOrder(event.transaction.hash.toHex() + '-' + event.logIndex.toString());
+  filledOrder.augustusRFQ = event.address;
+  filledOrder.augustusRFQVersion = "1.0.0";
+  filledOrder.orderHash = event.params.orderHash;
+  filledOrder.makerAddress = event.params.maker;
+  filledOrder.makerAsset = event.params.makerAsset;
+  filledOrder.makerAmount = event.params.makerAmount;
+  filledOrder.takerAddress = event.params.taker;
+  filledOrder.takerAsset = event.params.takerAsset;
+  filledOrder.takerAmount = event.params.takerAmount;
+  filledOrder.txHash = event.transaction.hash;
+  filledOrder.txOrigin = event.transaction.from;
+  filledOrder.txTarget = event.transaction.to;
+  filledOrder.txGasLimit = event.transaction.gasLimit;
+  filledOrder.txGasPrice = event.transaction.gasPrice;
+  filledOrder.blockHash = event.block.hash;
+  filledOrder.blockNumber = event.block.number;
+  filledOrder.timestamp = event.block.timestamp;
+  filledOrder.save();
 }
 
-export function handleOrderFilled(event: OrderFilled): void {}
+export function handleOrderFilledNFT(event: OrderFilledNFT): void {
+  let filledOrder = new FilledOrderNFT(event.transaction.hash.toHex() + '-' + event.logIndex.toString());
+  filledOrder.augustusRFQ = event.address;
+  filledOrder.augustusRFQVersion = "1.0.0";
+  filledOrder.orderHash = event.params.orderHash;
+  filledOrder.makerAddress = event.params.maker;
+  filledOrder.makerAsset = _getAssetAddress(event.params.makerAsset);
+  filledOrder.makerAssetType = _getAssetType(event.params.makerAsset);
+  filledOrder.makerAssetId = event.params.makerAssetId;
+  filledOrder.makerAmount = event.params.makerAmount;
+  filledOrder.takerAddress = event.params.taker;
+  filledOrder.takerAsset = _getAssetAddress(event.params.takerAsset);
+  filledOrder.takerAssetType = _getAssetType(event.params.takerAsset);
+  filledOrder.takerAssetId = event.params.takerAssetId;
+  filledOrder.takerAmount = event.params.takerAmount;
+  filledOrder.txHash = event.transaction.hash;
+  filledOrder.txOrigin = event.transaction.from;
+  filledOrder.txTarget = event.transaction.to;
+  filledOrder.txGasLimit = event.transaction.gasLimit;
+  filledOrder.txGasPrice = event.transaction.gasPrice;
+  filledOrder.blockHash = event.block.hash;
+  filledOrder.blockNumber = event.block.number;
+  filledOrder.timestamp = event.block.timestamp;
+  filledOrder.save();
+}
 
-export function handleOrderFilledNFT(event: OrderFilledNFT): void {}
+export function _getAssetType(asset: BigInt): BigInt {
+  let assetType = asset.rightShift(160);
+  if (assetType == BigInt.fromI32(0)) {
+    return BigInt.fromI32(20);
+  }
+  else if (assetType == BigInt.fromI32(1)) {
+    return BigInt.fromI32(1155);
+  }
+  else if (assetType == BigInt.fromI32(2)) {
+    return BigInt.fromI32(721);
+  }
+  else {
+    log.error("Unable to find asset type for asset {}", [asset.toHex()])
+    return BigInt.fromI32(0);
+  }
+}
+
+export function _getAssetAddress(asset: BigInt): Bytes {
+  return Bytes.fromHexString(asset.toHex().slice(0, 42)) as Bytes;
+}
